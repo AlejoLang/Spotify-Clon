0f13debbc3a7b3e61a19ef79ffb3c557cef37a51
Alejo Lang <alejolang123@gmail.com>
2022-11-18 23:20:18 -0300

    Integrate context and check and token chec check

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
+import { useEffect, useContext } from 'react';
 import './App.css';
+import { Route, Link, useLocation} from 'wouter'
+import { TokenContext } from './context/TokenContext';
+import Main from './pages/main'
+import Login from './pages/login'
+
 
 function App() {
+
+  const [path, goToPath] = useLocation();
+  const {token, setToken} = useContext(TokenContext)
+
+  useEffect(() => {
+    let token = window.localStorage.getItem('token');
+    const hash = window.location.hash;
+
+    if(!token && !hash) {
+      goToPath('/login')
+    }
+
+    if(!token && hash) {
+      token = hash.substring(1)
+                  .split("&")
+                  .find(elem => elem.startsWith("access_token"))
+                  .split("=")[1]
+      
+      window.location.hash = ''
+      window.localStorage.setItem('token', token);
+    }
+
+    setToken(token);
+  })
+
+  console.log(token)
+
   return (
     <div className="App">
-      
+      <Route path='/' component={Main}/>
+      <Route path='/login' component={Login}/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { TokenProvider } from './context/TokenContext';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <TokenProvider>
+      <App />
+    </TokenProvider>
   </React.StrictMode>
 );
 
