a97a42f064472dab1bb827b7a0011102a91d89f9
Alejo Lang <alejolang123@gmail.com>
2022-11-20 15:53:00 -0300

    Add sidebar and mobile styles

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import './styles.css'
+import { Link } from 'wouter'
+import { FaHome, FaSearch, FaHeart } from 'react-icons/fa'
+import { GiBookshelf } from 'react-icons/gi'
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+      <img src="/images/logos/Spotify-Logo.png" 
+          alt="Spotify Logo" 
+          className='spotifyLogo'
+      />
+      <div className="sidebarLinks">
+        <Link href='/'>
+          <FaHome />
+          Inicio
+        </Link>
+        <Link href='/search'>
+          <FaSearch />
+          Search
+        </Link>
+        <Link href='/playlists'>
+          <GiBookshelf />
+          Playlists
+        </Link>
+      </div>
+      <div className="sidebarOtherLinks">
+        <Link href='/playlists/liked'>
+          <FaHeart />
+          Liked
+        </Link>
+      </div>
+      <div className="sidebarPlaylists">
+        <Link href='/playlists/asd'>
+          asdas
+        </Link>
+        <Link href='/playlists/asd'>
+          asdas
+        </Link>
+        <Link href='/playlists/asd'>
+          asdas
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
