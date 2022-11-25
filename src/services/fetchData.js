c7a73632a9091c6d3cb53c898e1f8599aa4b8f88
Alejo Lang <alejolang123@gmail.com>
2022-11-25 20:32:30 -0300

    Fix errors

--- src/services/fetchData.js
+++ src/services/fetchData.js
@@ -2,11 +2,8 @@ const fetchData = async (url, body, method) => {
   const TOKEN = JSON.parse(localStorage.getItem('token'))?.access_token ?? undefined;
 
   if(!TOKEN){ console.log('err'); return;}
-
-  console.log(TOKEN, url, body, method);
-
   
-  let response = await fetch(url, {
+  let response = await fetch((url + `${method === 'GET' ? '?' + body : ''}`), {
       headers: {
         'Authorization': `Bearer ${TOKEN}`,
         'Content-Type': 'application/json'
